055180a5afd55cea77b11e9b3162f6f3cf026b22
Dibyendu Mandal <dibyendu.mandal@berkeley.edu>
2018-09-07 16:16:40 -0700

    oscillations again!

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+from wtforms import Form, FloatField, validators
+from math import pi
+import sys
+from numpy import exp, cos, linspace
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os, time, glob
+
+# model
+class InputForm(Form):
+    A = FloatField(
+        label='amplitude (m)', default=1.0,
+        validators=[validators.InputRequired()])
+    b = FloatField(
+        label='damping factor (kg/s)', default=0,
+        validators=[validators.InputRequired()])
+    w = FloatField(
+        label='frequency (1/s)', default=2*pi,
+        validators=[validators.InputRequired()])
+    T = FloatField(
+        label='time interval (s)', default=18,
+        validators=[validators.InputRequired()])
+
+# compute
+def damped_vibrations(t, A, b, w):
+    return A*exp(-b*t)*cos(w*t)
+
+def compute(A, b, w, T, resolution=500):
+    """Return filename of plot of the damped_vibration function."""
+    t = linspace(0, T, resolution+1)
+    u = damped_vibrations(t, A, b, w)
+    fig = plt.figure()  # needed to avoid adding curves in plot
+    plt.plot(t, u)
+    plt.title('A=%g, b=%g, w=%g' % (A, b, w))
+    if not os.path.isdir('static'):
+        os.mkdir('static')
+    else:
+        # Remove old plot files
+        for filename in glob.glob(os.path.join('static', '*.png')):
+            os.remove(filename)
+    # Use time since Jan 1, 1970 in filename in order make
+    # a unique filename that the browser has not chached
+    plotfile = os.path.join('static', str(time.time()) + '.png')
+    fig.savefig(plotfile)
+    return plotfile
+
+# view/control
+'''
+try:
+    template_name = sys.argv[1]
+except IndexError:
+    template_name = 'view4'
+'''
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        result = compute(form.A.data, form.b.data,
+                         form.w.data, form.T.data)
+    else:
+        result = None
+
+    return render_template('view4.html',
+                           form=form, result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
